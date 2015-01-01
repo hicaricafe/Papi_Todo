1931a3807a6a8bf6d4572924f425242816075d2a
hicaricafe <kazu@kazu-no-MacBook-Air.local>
2015-01-01 17:04:20 +0900

    First

---
+++ akabori_make_table のコピー 2.sql
@@ -0,0 +1,25 @@
+
+create database akabori;
+grant all on akabori.* to dbuser@localhost identified by "password";
+
+use akabori;
+
+create table tasks(
+	id int not null auto_increment primary key,
+	seq int not null,
+	type enum('notyet', 'done', 'deleted') default 'notyet',
+	title text,
+	created datetime,
+	modified datetime,
+	KEY type(type),
+	KEY seq(seq)
+);
+
+insert into tasks (seq, type, title, created, modified) values
+(1, 'notyet', '牛乳買う', now(), now()),
+(2, 'notyet', '提案書作る', now(), now()),
+(3, 'done', '映画見る', now(), now());
+
+
+
+
---
+++ html/tanaka/config.php
@@ -0,0 +1,11 @@
+<?php
+
+//DB
+
+define('DSN', 'mysql:host=localhost;dbname=akabori');
+define('DB_USER', 'dbuser');
+define('DB_PASSWORD', 'password');
+
+//エラー表示
+
+error_reporting(E_ALL & ~E_NOTICE);
